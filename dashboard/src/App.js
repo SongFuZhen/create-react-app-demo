d7dea9721c2623ffded960784a11d4232735ce7f
SongFuZhen <evil_song@126.com>
2017-06-12 18:48:13 +0800

    antd ui

---
+++ dashboard/src/App.js
@@ -0,0 +1,21 @@
+import React, { Component } from 'react';
+// import logo from './logo.svg';
+import './App.css';
+
+// import { Form, Icon, Input, Button, Checkbox } from 'antd';
+
+import { Form, Icon, Input, Checkbox, Row, Col, Button } from 'antd';
+
+class App extends Component {
+  render() {
+    return (
+      <div className="antd">
+          <Row>
+          
+          </Row>
+      </div>
+    )
+  }
+}
+
+export default App;
